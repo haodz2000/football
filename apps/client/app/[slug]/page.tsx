e14822a95937fcaf5137f76b8c024cf08f0c81e3
haodz2000 <tahuuhao1810@gmail.com>
2023-09-09 21:32:59 +0700

    fix: build

---
+++ apps/client/app/[slug]/page.tsx
@@ -0,0 +1,17 @@
+import { AppLayout } from '@client/components/layouts/AppLayout';
+import { Header } from '@client/components/layouts/headers/Header';
+import { Size } from '@client/components/theme/Size';
+import { Stack } from '@mui/material';
+
+export default async function Index() {
+  return (
+    <AppLayout
+      maxWidth={Size.maxWidth}
+      header={<Header maxWidth={Size.maxWidth} />}
+    >
+      <Stack width={1}>
+        Slug
+      </Stack>
+    </AppLayout>
+  );
+}
